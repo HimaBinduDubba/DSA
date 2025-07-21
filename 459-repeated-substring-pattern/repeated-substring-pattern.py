ac85626a26a62ea27e0c53194608d629db15e248
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-07-21 18:25:26 +0530

    Time: 115 ms (11.00%) | Memory: 17.9 MB (38.52%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        for i in range(len(s)-1):
+            
+                if s[0:i+1] * (len(s) // (i+1)) == s:
+                    return True
+        return False
