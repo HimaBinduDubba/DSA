0cd1438e191a29fe5557dfc4b188070d5ec90da0
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-07-24 19:25:35 +0530

    Time: 11 ms (31.21%) | Memory: 22.3 MB (36.76%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        target=len(nums)//3
+        freq={}
+        res=[]
+        for i in nums:
+            if i in freq:
+                freq[i]=freq[i]+1
+            else:
+                freq[i]=1
+        for key,value in freq.items():
+            if value>target:
+                res.append(key)
+        return res        
+
+
+
+
+
+        
