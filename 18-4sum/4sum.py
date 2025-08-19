247628536e5b2b175b1eca6ca1e35136785f55a3
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-08-19 18:30:49 +0530

    Time: 572 ms (16.49%) | Memory: 17.8 MB (55.36%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res=set()
+        nums.sort()
+        for i in range(len(nums)-3):
+            for j in range(i+1,len(nums)-2):
+              left=j+1
+              right=len(nums)-1
+              while left<right:
+                total=nums[i]+nums[left]+nums[right]+nums[j]
+                if total==target:
+                    res.add((nums[i],nums[left],nums[right],nums[j]))
+                    left=left+1
+                    right=right-1
+                elif total<target:
+                    left=left+1
+                else:
+                    right=right-1        
+        return list(res)
+        
