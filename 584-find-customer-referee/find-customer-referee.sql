c13b24382a01422470fb630ecf3deec3cfd8a6f4
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-11-26 18:16:09 +0530

    Time: 738 ms (5.21%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-(SELECT name FROM Customer WHERE referee_id <> 2) UNION ALL(SELECT name FROM Customer WHERE referee_id is Null)
+select name from Customer where referee_id!=2 or referee_id is null;
