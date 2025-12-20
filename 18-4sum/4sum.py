9b8add9d55df1ffd1af2b01e1837cab5284ffe74
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-12-20 14:35:55 +0530

    Time: 573 ms (15.46%) | Memory: 17.1 MB (99.95%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -4,17 +4,23 @@ class Solution:
         nums.sort()
         for i in range(len(nums)-3):
             for j in range(i+1,len(nums)-2):
-              left=j+1
-              right=len(nums)-1
-              while left<right:
-                total=nums[i]+nums[left]+nums[right]+nums[j]
-                if total==target:
-                    res.add((nums[i],nums[left],nums[right],nums[j]))
-                    left=left+1
-                    right=right-1
-                elif total<target:
-                    left=left+1
-                else:
-                    right=right-1        
-        return list(res)
+                left=j+1
+                right=len(nums)-1
+                while left<right:
+                    total=nums[i]+nums[j]+nums[left]+nums[right]
+                    if total==target:
+                        res.add((nums[i],nums[j],nums[left],nums[right]))
+                        left=left+1
+                        right=right-1
+                    elif total<target:
+                        left=left+1
+                    else:
+                        right=right-1
+        return list(res)                
+
+
+
+
+
+        
         
