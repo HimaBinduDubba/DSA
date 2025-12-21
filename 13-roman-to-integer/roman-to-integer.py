06001de1c99ea82ab647b3652ccb7985ae885f49
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-12-21 15:15:24 +0530

    Time: 3 ms (80.64%) | Memory: 17.2 MB (99.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,16 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        mapping={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        current_sum=0
-        prev_sum=0
+        map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        res=0
+        current=0
         for i in reversed(s):
-            char=mapping[i]
-            if char<prev_sum:
-                current_sum=current_sum-char
+            char=map[i]
+            if char<current:
+                res=res-char
             else:
-                current_sum=current_sum+char
-            prev_sum=char
-        return current_sum        
+                res=res+char
+            current=char
+        return res             
+
 
 
         
