64da9ca422e984d5f2466749d322966628f94b75
HimaBinduDubba <himabindu.dubba@gmail.com>
2025-12-22 11:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (99.83%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -7,8 +7,9 @@ class Solution:
             a=int(v1[i]) if i<len(v1) else 0
             b=int(v2[i]) if i<len(v2) else 0
             if a>b:
-                return 1
-            if a<b:
-                return -1
-        return 0        
+              return 1
+            elif a<b:
+              return -1
+        
+        return 0             
         
