2c5427f1d121b7f07ff8fadfa915f4e55267dcd5
HimaBinduDubba <himabindu.dubba@gmail.com>
2026-01-07 18:11:16 +0530

    Time: 11 ms (82.85%) | Memory: 23 MB (9.09%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-         res = [sorted(word) for word in strs]
-         visited = [False] * len(strs)
-         final = []
+        sorted_strs=[]
+        for i in range(len(strs)):
+            sorted_strs.append(tuple(sorted(strs[i])))
+        map={}
+        for i in range(len(strs)):
+            if sorted_strs[i] in map:
+                map[sorted_strs[i]].append(strs[i])
+            else:
+                map[sorted_strs[i]]=[strs[i]]
+        res=[]
+        for key in map:
+            res.append(map[key])
+        return res            
 
-         for i in range(len(strs)):
-            if not visited[i]:
-                group = [strs[i]]
-                visited[i] = True
-                for j in range(i + 1, len(strs)):
-                    if res[i] == res[j] and not visited[j]:
-                        group.append(strs[j])
-                        visited[j] = True
-                final.append(group)
-
-         return final
-
-
+        
