30eea7ae08926ec4445448683831c9d87d236117
HimaBinduDubba <himabindu.dubba@gmail.com>
2026-02-11 09:45:55 +0530

    Time: 1101 ms (68.56%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,1 +1,2 @@
-select EmployeeUNI.unique_id,Employees.name from Employees left join EmployeeUNI on Employees.id=EmployeeUNI.id;
+# Write your MySQL query statement below
+select EmployeeUNI.unique_id,Employees.name from Employees left join EmployeeUNI on Employees.id=EmployeeUNI.id
