006034400cc7722c68a79c1912899faf7475093f
HimaBinduDubba <himabindu.dubba@gmail.com>
2026-02-13 23:52:57 +0530

    Time: 7 ms (42.53%) | Memory: 19.4 MB (40.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        res=0
+        hash_map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         current=0
+        total=0
         for i in reversed(s):
-            char=map[i]
+            char=hash_map[i]
             if char<current:
-                res=res-char
+                total=total-char
             else:
-                res=res+char
+                total=total+char
             current=char
-        return res             
+        return total            
 
-
-
+        
         
