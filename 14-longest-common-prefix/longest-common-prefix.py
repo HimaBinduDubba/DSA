b8362d804b6e8fa95640575ee9e146df3cac51a1
HimaBinduDubba <himabindu.dubba@gmail.com>
2026-02-13 23:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (49.31%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,13 +3,13 @@ class Solution:
         if len(strs)==1:
             return strs[0]
         strs=sorted(strs)
-        i=0;j=0
+        i=0
+        j=0
         while i<len(strs[0]) and j<len(strs[-1]):
             if strs[0][i]!=strs[-1][j]:
-                
                 return strs[0][0:i]
-            else:
-                i=i+1
-                j=j+1
+            i=i+1
+            j=j+1
         return strs[0][0:i]            
-                
+
+        
