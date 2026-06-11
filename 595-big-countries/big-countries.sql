f4d3d563cd3cbf7942d218a2b31617bd6e04a4ac
Dubba Hima Bindu <himabindu.dubba@gmail.com>
2026-06-11 11:16:00 +0530

    Time: 332 ms (16.58%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-(SELECT name,population,area FROM World Where area>=3000000) Union  (SELECT name,population,area FROM World Where population>=25000000);
+select name,population,area from world where area>=3000000 or population>=25000000
