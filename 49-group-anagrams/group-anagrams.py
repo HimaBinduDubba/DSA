c947a58471d29406e55dee477afab7b68cd0e186
Dubba Hima Bindu <himabindu.dubba@gmail.com>
2026-08-04 08:29:42 +0530

    Time: 10 ms (86.73%) | Memory: 22.1 MB (52.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs=[]
-        for i in range(len(strs)):
-            sorted_strs.append(tuple(sorted(strs[i])))
         map={}
-        for i in range(len(strs)):
-            if sorted_strs[i] in map:
-                map[sorted_strs[i]].append(strs[i])
-            else:
-                map[sorted_strs[i]]=[strs[i]]
-        res=[]
-        for key in map:
-            res.append(map[key])
-        return res            
+        for str in strs:
+            key=''.join(sorted(str))
+            if key not in map:
+                map[key]=[]
+            
+            map[key].append(str)
+        return list(map.values())            
 
         
